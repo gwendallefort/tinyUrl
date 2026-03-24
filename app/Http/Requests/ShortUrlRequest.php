5af70a117084401432c12c48731727998ffba68c
Ted <tedgil+github@pm.me>
2026-03-24 18:24:21 +0100

    limit short code to 3 characters

--- app/Http/Requests/ShortUrlRequest.php
+++ app/Http/Requests/ShortUrlRequest.php
@@ -11,22 +11,22 @@ abstract class ShortUrlRequest extends FormRequest
     protected function commonRules(?int $excludeId = null): array
     {
         return [
-            'title'        => 'nullable|string|max:255',
+            'title' => 'nullable|string|max:255',
             'original_url' => [
                 'required', 'max:2048', 'regex:/^.+\..{2,}$/',
                 function ($attribute, $value, $fail) {
                     $shortCode = $this->input('short_code') ?: $this->route('shortUrl')?->short_code;
-                    if (!$shortCode) {
+                    if (! $shortCode) {
                         return;
                     }
                     $normalized = ShortUrl::setProtocolIfNotSet($value);
-                    if (rtrim($normalized, '/') === rtrim(url('/' . $shortCode), '/')) {
+                    if (rtrim($normalized, '/') === rtrim(url('/'.$shortCode), '/')) {
                         $fail(trans('validation_short_url.original_url.loop'));
                     }
                 },
             ],
-            'short_code'   => [
-                'nullable', 'string', 'min:2', 'max:20', 'alpha_dash',
+            'short_code' => [
+                'nullable', 'string', 'min:3', 'max:20', 'alpha_dash',
                 // avoid duplicate
                 function ($attribute, $value, $fail) use ($excludeId) {
                     $query = ShortUrl::whereRaw('BINARY short_code = ?', [$value]);
@@ -43,7 +43,7 @@ abstract class ShortUrlRequest extends FormRequest
                 function ($attribute, $value, $fail) {
                     $routeSegments = collect(Route::getRoutes())
                         ->map(fn ($route) => explode('/', ltrim($route->uri(), '/'))[0])
-                        ->filter(fn ($segment) => $segment && !str_starts_with($segment, '{'))
+                        ->filter(fn ($segment) => $segment && ! str_starts_with($segment, '{'))
                         ->unique()
                         ->values()
                         ->toArray();
