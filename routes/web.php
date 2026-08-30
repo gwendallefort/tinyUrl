48a738b15d32da6d12c4f738e0d3cb7c4c4b1fdb
Ted <tedgil+github@pm.me>
2026-08-30 15:29:45 +0200

    fix route auth middleware

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])
     ->middleware('guest')
     ->name('password.email');
 
-Route::middleware('auth')->middleware('throttle:30,1')->group(function () {
+Route::middleware(['auth', 'throttle:30,1'])->group(function () {
     // dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/email/verify', fn () => redirect()->route('dashboard'))->name('verification.notice');
